0fb0a4fece2ab3ffa6431f6512f70b5600b26d32
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2021-03-17 18:16:16 +0000

    Clamp function

---
+++ mathf/mathf.go
@@ -0,0 +1,14 @@
+package mathf
+
+// Clamp restricts the given value to the range defined by the given minimum and maximum values. Returns the
+// minimum if the given value is less than the minimum, or the maximum if it's greater.
+func Clamp(v, minimum, maximum float64) float64 {
+	if v > maximum {
+		return maximum
+	}
+	if v < minimum {
+		return minimum
+	}
+
+	return v
+}
